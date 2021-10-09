8061b30930daafdb497ecc1de87150536cce6f06
stepurko <stepurko@vbankcenter.ru>
2021-10-09 14:27:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/moviedbapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.moviedbapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviedbapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
