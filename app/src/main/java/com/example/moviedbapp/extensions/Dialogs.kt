721ed2caaeeb357aee72e37c1f92a77257b4f7e2
stepurko <stepurko@vbankcenter.ru>
2022-01-02 19:44:11 +0300

    Added Dialog extensions

---
+++ app/src/main/java/com/example/moviedbapp/extensions/Dialogs.kt
@@ -0,0 +1,38 @@
+package com.example.moviedbapp.extensions
+
+import android.content.Context
+import android.content.DialogInterface
+import com.example.moviedbapp.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+fun showAlertDialog(
+    context: Context,
+    title: String,
+    message: String,
+    neutralButtonText: String? = null,
+    neutralButtonListener: DialogInterface.OnClickListener? = null,
+    negativeButtonText: String? = null,
+    negativeButtonListener: DialogInterface.OnClickListener? = null,
+    positiveButtonText: String? = null,
+    positiveButtonListener: DialogInterface.OnClickListener? = null
+) {
+    MaterialAlertDialogBuilder(context)
+        .setTitle(title)
+        .setMessage(message)
+        .setNeutralButton(neutralButtonText, neutralButtonListener)
+        .setNegativeButton(negativeButtonText, negativeButtonListener)
+        .setPositiveButton(positiveButtonText, positiveButtonListener)
+        .show()
+}
+
+fun showErrorLoadingDialog(
+    context: Context
+) {
+    showAlertDialog(
+        context,
+        title = context.getString(R.string.error_loading_data_title),
+        message = context.getString(R.string.error_loading_data_message),
+        positiveButtonText = context.getString(R.string.error_loading_data_positive),
+        positiveButtonListener = { dialog, _ -> dialog.dismiss() }
+    )
+}
