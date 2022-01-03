ab8577c28ac612b1f992dd32f9e51ee8bd250dc1
stepurko <stepurko@vbankcenter.ru>
2022-01-03 14:28:19 +0300

    Added service to AndroidManifest.xml Added parcelize kotlin plugin Refactored MovieDetailsFragment.kt for IntentService HTTP query Added MovieDetailsService.kt

--- app/src/main/java/com/example/moviedbapp/ui/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/moviedbapp/ui/MovieDetailsFragment.kt
@@ -1,38 +1,53 @@
 package com.example.moviedbapp.ui
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.moviedbapp.BuildConfig
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.moviedbapp.R
 import com.example.moviedbapp.databinding.FragmentMovieDetailsBinding
-import com.example.moviedbapp.extensions.showErrorLoadingDialog
+import com.example.moviedbapp.extensions.showAlertDialog
 import com.example.moviedbapp.model.entities.MovieDetailsDTO
-import com.example.moviedbapp.model.entities.MovieListItem
-import com.example.moviedbapp.viewmodel.AppState
-import com.example.moviedbapp.viewmodel.MovieDetailsViewModel
-import com.google.gson.Gson
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.lang.Exception
-import java.net.MalformedURLException
-import java.net.URL
-import java.util.stream.Collectors
-import javax.net.ssl.HttpsURLConnection
+import com.example.moviedbapp.service.MovieDetailsService
 
 class MovieDetailsFragment : Fragment(R.layout.fragment_movie_details) {
 
-    private lateinit var viewModel: MovieDetailsViewModel
     private var _binding: FragmentMovieDetailsBinding? = null
     private val binding get() = _binding!!
+
+    private val loadResultReceiver: BroadcastReceiver = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (intent?.getStringExtra(MOVIE_DETAILS_LOAD_RESULT)) {
+                MOVIE_DETAILS_EMPTY_INTENT -> {
+                    showAlertDialog(this@MovieDetailsFragment.requireContext(), "Ошибка загрузки данных", MOVIE_DETAILS_EMPTY_INTENT)
+                }
+                MOVIE_DETAILS_EMPTY_INPUT_DATA -> {
+                    showAlertDialog(this@MovieDetailsFragment.requireContext(), "Ошибка загрузки данных", MOVIE_DETAILS_EMPTY_INPUT_DATA)
+                }
+                MOVIE_DETAILS_ERROR -> {
+                    val errorMessage = intent.getStringExtra(MOVIE_DETAILS_ERROR_MESSAGE) ?: "Неизвестная ошибка"
+                    showAlertDialog(this@MovieDetailsFragment.requireContext(), "Ошибка загрузки данных", errorMessage)
+                }
+                MOVIE_DETAILS_MALFORMED_URL -> {
+                    showAlertDialog(this@MovieDetailsFragment.requireContext(), "Ошибка загрузки данных", MOVIE_DETAILS_MALFORMED_URL)
+                }
+                MOVIE_DETAILS_RESPONSE_SUCCESS -> {
+                    val movieDetails = intent.getParcelableExtra<MovieDetailsDTO>(
+                        MOVIE_DETAILS_EXTRA)
+                    displayDetails(movieDetails)
+                }
+            }
+        }
+
+    }
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
@@ -40,88 +55,51 @@ class MovieDetailsFragment : Fragment(R.layout.fragment_movie_details) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val id = arguments?.getInt(ID) ?: -1
+        val id = arguments?.getInt(MOVIE_ID) ?: -1
         _binding = FragmentMovieDetailsBinding.inflate(inflater, container, false).apply {
-            initVm()
-            initObservers()
+            registerReceiver(loadResultReceiver)
             requestData(550)
         }
         return binding.root
     }
 
-    private fun FragmentMovieDetailsBinding.initVm() {
-        viewModel = ViewModelProvider(this@MovieDetailsFragment)[MovieDetailsViewModel::class.java]
+    private fun registerReceiver(loadResultReceiver: BroadcastReceiver) {
+        LocalBroadcastManager.getInstance(requireContext()).registerReceiver(loadResultReceiver, IntentFilter(
+            MOVIE_DETAILS_INTENT_FILTER))
     }
 
-    private fun FragmentMovieDetailsBinding.initViews(data: MovieListItem.MovieData) {
-
-    }
-
-    private fun FragmentMovieDetailsBinding.initObservers() {
-        viewModel.liveData.observe(viewLifecycleOwner, {
-            when (it) {
-                is AppState.Loading -> {
-                    progress.isVisible = true
-                }
-                is AppState.Error -> {
-                    progress.isVisible = false
-                    showErrorLoadingDialog(requireContext())
-                }
-                is AppState.Success -> {
-                    progress.isVisible = false
-                    initViews(it.data)
-                }
-            }
-        }
-        )
-    }
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun FragmentMovieDetailsBinding.requestData(id: Int) {
-        try {
-            val uri = URL("https://api.themoviedb.org/3/movie/$id?api_key=${BuildConfig.API_KEY_V3}&language=en-US")
-            val handler = Handler()
-            Thread {
-                lateinit var urlConnection: HttpsURLConnection
-                try {
-                    urlConnection = uri.openConnection() as HttpsURLConnection
-                    urlConnection.requestMethod = "GET"
-                    urlConnection.readTimeout = 10000
-                    Log.d("SAY", "${urlConnection.responseCode}")
-                    val bufferedReader =
-                        BufferedReader(InputStreamReader(urlConnection.inputStream))
-                    val movieDetails: MovieDetailsDTO =
-                        Gson().fromJson(getLines(bufferedReader), MovieDetailsDTO::class.java)
-                    handler.post {
-                        displayDetails(movieDetails)
-                    }
-                } catch (e: Exception) {
-                    Log.e("", "Fail connection", e)
-                    e.printStackTrace()
-                } finally {
-                    urlConnection.disconnect()
-                }
-            }.start()
-        } catch (e: MalformedURLException) {
-            Log.e("","Failed URI", e)
-            e.printStackTrace()
+    private fun requestData(id: Int) {
+        context?.let {
+            it.startService(Intent(it, MovieDetailsService::class.java).apply {
+                putExtra(MOVIE_ID, id)
+            })
         }
     }
 
-    private fun displayDetails(movieDetails: MovieDetailsDTO) {
+    private fun displayDetails(movieDetails: MovieDetailsDTO?) {
         with(binding) {
-            tvName.text = movieDetails.title
+            tvName.text = movieDetails?.title ?: "-"
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun getLines(reader: BufferedReader): String {
-        return reader.lines().collect(Collectors.joining("\n"))
+    override fun onDestroy() {
+        LocalBroadcastManager.getInstance(requireContext()).unregisterReceiver(loadResultReceiver)
+        super.onDestroy()
     }
 
     companion object {
 
-        const val ID = "ID"
+        const val MOVIE_ID = "MOVIE_ID"
+        const val MOVIE_DETAILS_INTENT_FILTER = "MOVIE_DETAILS_INTENT_FILTER"
+        const val MOVIE_DETAILS_RESPONSE_SUCCESS = "MOVIE_DETAILS_RESPONSE_SUCCESS"
+        const val MOVIE_DETAILS_EXTRA = "MOVIE_DETAILS_TITLE"
+        const val MOVIE_DETAILS_EMPTY_INTENT = "MOVIE_DETAILS_EMPTY_INTENT"
+        const val MOVIE_DETAILS_EMPTY_INPUT_DATA = "MOVIE_DETAILS_EMPTY_INPUT_DATA"
+        const val MOVIE_DETAILS_ERROR = "MOVIE_DETAILS_ERROR"
+        const val MOVIE_DETAILS_ERROR_MESSAGE = "MOVIE_DETAILS_ERROR_MESSAGE"
+        const val MOVIE_DETAILS_LOAD_RESULT = "MOVIE_DETAILS_LOAD_RESULT"
+        const val MOVIE_DETAILS_MALFORMED_URL = "MOVIE_DETAILS_MALFORMED_URL"
+
 
         fun newInstance(bundle: Bundle): MovieDetailsFragment {
             val fragment = MovieDetailsFragment()
