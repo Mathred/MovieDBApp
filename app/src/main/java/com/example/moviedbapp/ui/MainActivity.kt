774c650abf7f30bd4ee703d0fafea6d851ffce5e
stepurko <stepurko@vbankcenter.ru>
2022-01-05 14:15:29 +0300

    Added NavigationView with TopAppBar

--- app/src/main/java/com/example/moviedbapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviedbapp/ui/MainActivity.kt
@@ -2,17 +2,42 @@ package com.example.moviedbapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
 import com.example.moviedbapp.R
+import com.example.moviedbapp.databinding.MainActivityBinding
+import com.example.moviedbapp.utils.openHome
+import com.example.moviedbapp.utils.openSettings
 
 class MainActivity : AppCompatActivity() {
 
+    private var _binding: MainActivityBinding? = null
+    private val binding: MainActivityBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        _binding = MainActivityBinding.inflate(layoutInflater).apply {
+            setupTopAppBar()
+        }
+        setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+    }
+
+    private fun MainActivityBinding.setupTopAppBar() {
+        topAppBar.setNavigationOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> this@MainActivity.openHome()
+                R.id.settings -> this@MainActivity.openSettings()
+            }
+            menuItem.isChecked = true
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
         }
     }
 }
