ca355166ecfc5a2c8e8affc8f7f6f83d8ffe7e6a
stepurko <stepurko@vbankcenter.ru>
2022-01-05 14:17:58 +0300

    Added SettingsFragment.kt Added openHome() and openSettings() to Navigator. Relocated Navigator.kt. Animation corrections to Navigator.kt

---
+++ app/src/main/java/com/example/moviedbapp/utils/Navigator.kt
@@ -0,0 +1,55 @@
+package com.example.moviedbapp.utils
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import com.example.moviedbapp.ui.MovieDetailsFragment
+import com.example.moviedbapp.R
+import com.example.moviedbapp.ui.MainFragment
+import com.example.moviedbapp.ui.SettingsFragment
+
+
+fun Fragment.openMovie(
+    id: Int
+) {
+    val bundle = bundleOf(
+        MovieDetailsFragment.MOVIE_ID to id
+    )
+    this
+        .activity
+        ?.supportFragmentManager
+        ?.beginTransaction()
+        ?.setCustomAnimations(
+            R.anim.enter_from_right,
+            R.anim.exit_to_right,
+            R.anim.enter_from_right,
+            R.anim.exit_to_right,
+        )
+        ?.add(R.id.container, MovieDetailsFragment.newInstance(bundle))
+        ?.addToBackStack("")
+        ?.commit()
+}
+
+fun AppCompatActivity.openHome() {
+    this.supportFragmentManager
+        .beginTransaction()
+        .setCustomAnimations(
+            R.anim.enter_from_right,
+            R.anim.exit_to_left,
+        )
+        .replace(R.id.container, MainFragment.newInstance())
+        .addToBackStack("")
+        .commit()
+}
+
+fun AppCompatActivity.openSettings() {
+    this.supportFragmentManager
+        .beginTransaction()
+        .setCustomAnimations(
+            R.anim.enter_from_right,
+            R.anim.exit_to_left,
+        )
+        .replace(R.id.container, SettingsFragment.newInstance())
+        .addToBackStack("")
+        .commit()
+}
